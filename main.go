0b0e044ddcca89a247d76a6f0c0cc1abf718face
Oussama_Wahbi <wahbi.oussama08@gmail.com>
2026-06-05 20:52:54 +0100

    update

--- main.go
+++ main.go
@@ -34,16 +34,19 @@ func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	reader := bufio.NewReader(conn)
-	message, err := reader.ReadString('\n')
-	if err != nil {
-		log.Printf("Read error: %v", err)
-		return
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			log.Printf("Read error: %v", err)
+			break
+		}
+	
+		ackMsg := strings.ToUpper(strings.TrimSpace(message))
+		response := fmt.Sprintf("ACK: %s\n", ackMsg)
+		_, err = conn.Write([]byte(response))
+		if err != nil {
+			log.Printf("Server write error: %v", err)
+			break
+		}
 	}
-
-	ackMsg := strings.ToUpper(strings.TrimSpace(message))
-    response := fmt.Sprintf("ACK: %s\n", ackMsg)
-    _, err = conn.Write([]byte(response))
-    if err != nil {
-        log.Printf("Server write error: %v", err)
-    }
 }
