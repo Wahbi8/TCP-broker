f91b6bffffd131ca463db29734ce276234626864
Oussama_Wahbi <wahbi.oussama08@gmail.com>
2026-06-08 21:51:00 +0100

    update

--- consumer/main.go
+++ consumer/main.go
@@ -1,2 +1,40 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: consumer <topic>")
+		return
+	}
+
+	topic := os.Args[1]
+
+	conn, err := net.Dial("tcp", ":8090")
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		return
+	}
+	defer conn.Close()
+
+	_, err = conn.Write([]byte(fmt.Sprintf("SUB %s\n", topic)))
+	if err != nil {
+		fmt.Println("Error subscribing:", err)
+		return
+	}
+
+	reader := bufio.NewReader(conn)
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Connection closed:", err)
+			return
+		}
+		fmt.Print(msg)
+	}
+}
--- producer/main.go
+++ producer/main.go
@@ -1,2 +1,41 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: producer <topic> <message>")
+		return
+	}
+
+	topic := os.Args[1]
+	message := strings.Join(os.Args[2:], " ")
+
+	conn, err := net.Dial("tcp", ":8090")
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		return
+	}
+	defer conn.Close()
+
+	payload := fmt.Sprintf("PUB %s %s\n", topic, message)
+	_, err = conn.Write([]byte(payload))
+	if err != nil {
+		fmt.Println("Error sending:", err)
+		return
+	}
+
+	ack, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+
+	fmt.Print("Broker response: ", ack)
+}
